3f80dbb8334ea76be09ebb459a903ff0a648b779
Vincent27_ <vincent.miras03@gmail.com>
2025-03-28 14:30:21 +0100

    made the link to the file local

--- python/client.py
+++ python/client.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, render_template
 import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -13,7 +14,9 @@ def home():
 def run_bash_command():
     try:
         # Exemple de commande Bash
-        result = subprocess.run(["/home/formation/projet/LamAPI/python/run-oscp-gpp-client.sh"], capture_output=True, text=True)
+        CUR_DIR = os.getcwd()
+        bash_script_path = os.path.join(CUR_DIR, 'run-oscp-gpp-client.sh')
+        result = subprocess.run([bash_script_path], capture_output=True, text=True)
         return jsonify({'output': result.stdout.strip()})
     except Exception as e:
         return jsonify({'error': str(e)})
