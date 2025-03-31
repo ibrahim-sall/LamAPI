1ee790911f77319c4a9708350974fabffb28cd9c
LBilheran <loisbilheran@outlook.fr>
2025-03-31 10:22:47 +0200

    code resilient to lack of file

--- python/demo_client.py
+++ python/demo_client.py
@@ -14,6 +14,7 @@
 from PIL import Image
 from oscp.geoposeprotocol import *
 import time
+import os
 import json
 import base64
 import requests
@@ -75,66 +76,119 @@ parser.add_argument(
 args=parser.parse_args()
 
 
-with open(args.image, 'rb') as f:
-    image = f.read()
-    image_base64 = base64.b64encode(image).decode('utf-8')
-    f.close()
+def check_file(path, name):
+    if not os.path.isfile(path):
+        print(f"Erreur : le fichier {name} n'existe pas à l'emplacement : {path}")
+        return False
+    return True
 
-# open it again with PIL just to find out its size
-image = Image.open(args.image)
+geoPoseRequest = GeoPoseRequest()
+geoPoseRequest.timestamp = datetime.now(timezone.utc).timestamp()*1000 # milliseconds since epoch
 
-with open(args.imagestxt, 'r') as f:
-    lines = f.read().splitlines()[1:]  # skip header
-    f.close()
+if not check_file(args.image, "image"):
+    sys.exit(1)
+else :   
+    with open(args.image, 'rb') as f:
+        image = f.read()
+        image_base64 = base64.b64encode(image).decode('utf-8')
+        f.close()
 
-images_config = [line.strip().split(', ') for line in lines][0]
-print("Image config:")
-print(images_config)
+    # open it again with PIL just to find out its size
+    image = Image.open(args.image)
 
-with open(args.sensors, 'r') as f:
-    lines = f.read().splitlines()[1:]
-    f.close()
+if  check_file(args.imagestxt, "imagestxt"):
+    with open(args.imagestxt, 'r') as f:
+        lines = f.read().splitlines()[1:]  # skip header
+        f.close()
 
-sensors_config = [line.strip().split(', ') for line in lines]
-print("Sensors config:")
-print(sensors_config)
+    images_config = [line.strip().split(', ') for line in lines][0]
+    print("Image config:")
+    print(images_config)
 
-with open(args.bt, 'r') as f:
-    lines = f.read().splitlines()[1:]
-    f.close()
+    kCameraSensorId = images_config[1]
+    cameraReading = CameraReading(sensorId=kCameraSensorId)
+    cameraReading.timestamp = images_config[0]
+    cameraReading.imageFormat = ImageFormat.RGBA32
+    cameraReading.size = [image.width, image.height]
+    cameraReading.imageBytes = image_base64
+    cameraReading.sequenceNumber = 1
+    cameraReading.imageOrientation = ImageOrientation()
 
-bt_config = [line.strip().split(', ') for line in lines]
-print("Bluetooth config:")
-print(bt_config)
+else :
+    cameraReading = CameraReading(sensorId="ios_2025-04-01_09.00.00_000/cam_phone_00000000001")
+    cameraReading.timestamp = geoPoseRequest.timestamp
+    cameraReading.imageFormat = ImageFormat.RGBA32
+    cameraReading.size = [image.width, image.height]
+    cameraReading.imageBytes = image_base64
+    cameraReading.sequenceNumber = 1
+    cameraReading.imageOrientation = ImageOrientation()
 
-with open(args.wifi, 'r') as f:
-    lines = f.read().splitlines()[1:]
-    f.close()
+if check_file(args.sensors, "sensors"):
+    with open(args.sensors, 'r') as f:
+        lines = f.read().splitlines()[1:]
+        f.close()
 
-wifi_config = [line.strip().split(', ') for line in lines]
-print("Wifi config:")
-print(wifi_config)
+    sensors_config = [line.strip().split(', ') for line in lines]
+    print("Sensors config:")
+    print(sensors_config)
 
-with open(args.trajectories, 'r') as f:
-    lines = f.read().splitlines()[1:]
-    f.close()
+    cameraReading.params.model = sensors_config[0][3]
+    cameraReading.params.modelParams = sensors_config[0][4:]
 
-trajectories_config = [line.strip().split(', ') for line in lines]
-print("Trajectories config:")
-print(trajectories_config)
+    geoPoseRequest.sensors.append(Sensor(type = SensorType.CAMERA, id=kCameraSensorId, name=sensors_config[0][1], model=sensors_config[0][3]))
+    geoPoseRequest.sensorReadings.cameraReadings.append(cameraReading)
+
+if check_file(args.bt, "bt"):
+    with open(args.bt, 'r') as f:
+        lines = f.read().splitlines()[1:]
+        f.close()
+
+    bt_config = [line.strip().split(', ') for line in lines]
+    print("Bluetooth config:")
+    print(bt_config)
+
+    kBluetoothSensorId = bt_config[0][1]
+    bluetoothReading = BluetoothReading(sensorId=kBluetoothSensorId)
+    bluetoothReading.timestamp = bt_config[0][0]
+    bluetoothReading.address = [bt_config[i][2] for i in range(0,len(bt_config))]
+    bluetoothReading.RSSI = [bt_config[i][3] for i in range(0,len(bt_config))]
+
+    geoPoseRequest.sensors.append(Sensor(type = SensorType.BLUETOOTH, id=kBluetoothSensorId))
+    geoPoseRequest.sensorReadings.bluetoothReadings.append(bluetoothReading)
+
+if check_file(args.wifi, "wifi"):
+    with open(args.wifi, 'r') as f:
+        lines = f.read().splitlines()[1:]
+        f.close()
+
+    wifi_config = [line.strip().split(', ') for line in lines]
+    print("Wifi config:")
+    print(wifi_config)
+
+    kWifiSensorId = wifi_config[0][1]
+    wifiReading = WiFiReading(sensorId=kWifiSensorId)
+    wifiReading.timestamp = wifi_config[0][0]
+    wifiReading.BSSID = [wifi_config[i][2] for i in range(0,len(wifi_config))]
+    wifiReading.frequency = [wifi_config[i][3] for i in range(0,len(wifi_config))]
+    wifiReading.RSSI = [wifi_config[i][4] for i in range(0,len(wifi_config))]
+    wifiReading.scanTimeStart = [wifi_config[i][6] for i in range(0,len(wifi_config))]
+    wifiReading.scanTimeEnd = [wifi_config[i][7] for i in range(0,len(wifi_config))]
+
+    geoPoseRequest.sensors.append(Sensor(type = SensorType.WIFI, id=kWifiSensorId))
+    geoPoseRequest.sensorReadings.wifiReadings.append(wifiReading)
+
+if check_file(args.trajectories, "trajectories"):
+    with open(args.trajectories, 'r') as f:
+        lines = f.read().splitlines()[1:]
+        f.close()
+
+    trajectories_config = [line.strip().split(', ') for line in lines]
+    print("Trajectories config:")
+    print(trajectories_config)
 
 
-kCameraSensorId = images_config[1]
-cameraReading = CameraReading(sensorId=kCameraSensorId)
-cameraReading.timestamp = images_config[0]
-cameraReading.sensorId = kCameraSensorId
-cameraReading.imageFormat = ImageFormat.RGBA32
-cameraReading.size = [image.width, image.height]
-cameraReading.imageBytes = image_base64
-cameraReading.sequenceNumber = 1
-cameraReading.imageOrientation = ImageOrientation()
-cameraReading.params.model = sensors_config[0][3]
-cameraReading.params.modelParams = sensors_config[0][4:]
+
+
 # cameraReading.params = CameraParameters(model=camera_config["camera_model"], modelParams=camera_config["camera_params"])
 
 # kGeolocationSensorId = "my_gps_sensor"
@@ -144,29 +198,6 @@ cameraReading.params.modelParams = sensors_config[0][4:]
 # geolocationReading.longitude = geolocation_config["lon"]
 # geolocationReading.altitude = geolocation_config["h"]
 
-kBluetoothSensorId = bt_config[0][1]
-bluetoothReading = BluetoothReading(sensorId=kBluetoothSensorId)
-bluetoothReading.timestamp = bt_config[0][0]
-bluetoothReading.address = [bt_config[i][2] for i in range(0,len(bt_config))]
-bluetoothReading.RSSI = [bt_config[i][3] for i in range(0,len(bt_config))]
-
-kWifiSensorId = bt_config[0][1]
-wifiReading = WiFiReading(sensorId=kWifiSensorId)
-wifiReading.timestamp = wifi_config[0][0]
-wifiReading.BSSID = [wifi_config[i][2] for i in range(0,len(wifi_config))]
-wifiReading.frequency = [wifi_config[i][3] for i in range(0,len(wifi_config))]
-wifiReading.RSSI = [wifi_config[i][4] for i in range(0,len(wifi_config))]
-wifiReading.scanTimeStart = [wifi_config[i][6] for i in range(0,len(wifi_config))]
-wifiReading.scanTimeEnd = [wifi_config[i][7] for i in range(0,len(wifi_config))]
-
-geoPoseRequest = GeoPoseRequest()
-geoPoseRequest.timestamp = datetime.now(timezone.utc).timestamp()*1000 # milliseconds since epoch
-geoPoseRequest.sensors.append(Sensor(type = SensorType.CAMERA, id=kCameraSensorId, name=sensors_config[0][1], model=sensors_config[0][3]))
-geoPoseRequest.sensorReadings.cameraReadings.append(cameraReading)
-geoPoseRequest.sensors.append(Sensor(type = SensorType.BLUETOOTH, id=kBluetoothSensorId))
-geoPoseRequest.sensorReadings.bluetoothReadings.append(bluetoothReading)
-geoPoseRequest.sensors.append(Sensor(type = SensorType.WIFI, id=kWifiSensorId))
-geoPoseRequest.sensorReadings.wifiReadings.append(wifiReading)
 # geoPoseRequest.sensors.append(Sensor(type = SensorType.GEOLOCATION, id=kGeolocationSensorId))
 # geoPoseRequest.sensorReadings.geolocationReadings.append(geolocationReading)
 
