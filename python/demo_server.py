f8e24004b58adbd4e22aa91ce51deafcb12be161
LBilheran <loisbilheran@outlook.fr>
2025-04-14 13:38:09 +0200

    patch volume and conversion arguments

--- python/demo_server.py
+++ python/demo_server.py
@@ -117,7 +117,7 @@ def localize():
     geoPose.quaternion.w = last_line[2]
     ###Convertt to WGS84
   
-    geoPose.position.lat, geoPose.position.lon,  geoPose.position.h  = convert_to_wgs84(float(last_line[6]), float(last_line[7]), float(last_line[8]))
+    geoPose.position.lat, geoPose.position.lon,  geoPose.position.h  = convert_to_wgs84(np.array(float(last_line[6]), float(last_line[7]), float(last_line[8])))
 
     geoPoseResponse = GeoPoseResponse(id = geoPoseRequest.id, timestamp = geoPoseRequest.timestamp)
     geoPoseResponse.geopose = geoPose
--- python/server.sh
+++ python/server.sh
@@ -3,4 +3,4 @@ set -e
 
 pip install --no-cache-dir flask requests pillow tqdm numpy pyproj laspy flask_swagger_ui docker
 
-exec python3 /app/demo_server.py --output_path=output_volume
+exec python3 /app/demo_server.py --output_path=/output
